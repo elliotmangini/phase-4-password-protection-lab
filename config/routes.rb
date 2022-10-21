e6efee446fa6a464675d829410ea64dd6b6da5c6
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-21 10:40:28 -0600

    commit tree 8d12408897425df2cdff82626b34e5efc87425cb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
